882b08390ad4c92ddc596fc5c6f2301423018260
Vito Minheere <10959946+VitoMinheere@users.noreply.github.com>
2023-12-08 14:57:19 +0100

    Move endpoints to different folder

--- flask_tasks/app.py
+++ flask_tasks/app.py
@@ -1,64 +1,15 @@
-from flask import Flask, request, jsonify, make_response
-from flask_restful import Resource, Api
-from pydantic import ValidationError
+from flask import Flask
+from flask_restful import Api
 
-from flask_tasks.models import TaskCreateUpdateModel, TaskResponseModel
+from .endpoints.tasks import TaskResource, TaskDetailResource
 
 app = Flask(__name__)
 api = Api(app)
 
-# DB for quick tests
-tasks = [{"id": 1, "title": "First Task", "description": "Test"}]
-task_id_counter = 1
-
-# Task Resource
-class TaskResource(Resource):
-    def get(self):
-        return jsonify({'tasks': [task for task in tasks]})
-
-    def post(self):
-        try:
-            data = TaskCreateUpdateModel(**request.json)
-            task = {'id': len(tasks)+1, **data.model_dump()}
-            tasks.append(task)
-            return jsonify({'task': task})
-        except ValidationError as e:
-            return make_response(jsonify({'error': e.errors()}), 400)
-
-class TaskDetailResource(Resource):
-    def get(self, task_id):
-        task = next((t for t in tasks if t['id'] == task_id), None)
-
-        if task:
-            return jsonify({'task': TaskResponseModel(**task).model_dump()})
-
-        return make_response(jsonify({'error': 'Task not found'}), 404)
-
-    def put(self, task_id):
-        task = next((t for t in tasks if t['id'] == task_id), None)
-        if task:
-            try:
-                data = TaskCreateUpdateModel(**request.json)
-            except ValidationError as e:
-                return make_response(jsonify({'error': e.errors()}), 400)
-            else:
-                task.update(data.model_dump())
-                return jsonify({'task': task})
-
-        return make_response(jsonify({'error': 'Task not found'}), 404)
-
-    def delete(self, task_id):
-        global tasks
-        task = next((t for t in tasks if t['id'] == task_id), None)
-        if not task:
-            return make_response(jsonify({'error': 'Task not found'}), 404)
-
-        tasks = [t for t in tasks if t['id'] != task_id]
-        return make_response(jsonify({'message': 'Task deleted successfully'}))
 
 # Add resources to the API
-api.add_resource(TaskResource, '/tasks')
-api.add_resource(TaskDetailResource, '/tasks/<int:task_id>')
+api.add_resource(TaskResource, "/tasks")
+api.add_resource(TaskDetailResource, "/tasks/<int:task_id>")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
