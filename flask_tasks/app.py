be3edd6bc47a2f8a5616f26f2183ea0f2520166b
Vito Minheere <10959946+VitoMinheere@users.noreply.github.com>
2023-12-08 21:05:26 +0100

    Move all code to app.py for easier development

--- flask_tasks/app.py
+++ flask_tasks/app.py
@@ -1,15 +1,137 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
-from .endpoints.tasks import TaskResource, TaskDetailResource
+from flask import request, jsonify, make_response
+from flask_restful import Resource, reqparse
+
+# from .endpoints.tasks import TaskResource, TaskDetailResource
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import text
 
 app = Flask(__name__)
 api = Api(app)
 
+# WEBSITE_HOSTNAME exists only in production environment
+if 'WEBSITE_HOSTNAME' not in os.environ:
+    # local development, where we'll use environment variables
+    print("Loading config.development and environment variables from .env file.")
+    app.config.from_object('azureproject.development')
+else:
+    # production
+    print("Loading config.production.")
+    app.config.from_object('azureproject.production')
 
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+# Initialize the database connection
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=text("uuid_generate_v4()"))
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    created_at = db.Column(db.DateTime())
+
+    def __repr__(self):
+        return self.title
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "description": self.description
+        }
+
+
+# DB for quick tests
+tasks = [{"id": 1, "title": "First Task", "description": "Test"}]
+
+# Task Resource
+class TaskResource(Resource):
+    """Endpoint for getting and creating tasks."""
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, required = True, help = 'No task title provided')
+        self.reqparse.add_argument('description', type = str, required=True, location = 'json')
+        super(TaskResource, self).__init__()
+
+    def get(self):
+        """Return all tasks found in database."""
+        return jsonify({"tasks": [task for task in tasks]})
+
+    def post(self):
+        try:
+            d = {}
+            args = self.reqparse.parse_args()
+            print(args)
+            for k, v in args.items():
+                if v != None:
+                    d[k] = v
+
+            data = Task(**d)
+            task = {"id": len(tasks) + 1, **data.to_dict()}
+            tasks.append(task)
+            return jsonify({"task": task})
+        except TypeError as e:
+            return make_response(jsonify({"error": str(e)}), 400)
+
+
+class TaskDetailResource(Resource):
+    """Endpoint for getting, updating and deleting a single task."""
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, location = 'json')
+        self.reqparse.add_argument('description', type = str, location = 'json')
+        super(TaskDetailResource, self).__init__()
+    
+
+    def get(self, task_id: int):
+        task = next((t for t in tasks if t["id"] == task_id), None)
+
+        if task:
+            return jsonify({"task": Task(**task).to_dict()})
+
+        return make_response(jsonify({"error": "Task not found"}), 404)
+
+    def put(self, task_id: int):
+        task = next((t for t in tasks if t["id"] == task_id), None)
+        if task:
+            try:
+                # data = Task(**request.json) 
+                data = {}
+                args = self.reqparse.parse_args()
+                for k, v in args.items():
+                    if v != None:
+                        data[k] = v
+            except TypeError as e:
+                return make_response(jsonify({"error": e.errors()}), 400)
+            else:
+                task.update(data)
+                return jsonify({"task": task})
+
+        return make_response(jsonify({"error": "Task not found"}), 404)
+
+    def delete(self, task_id: int):
+        global tasks
+        task = next((t for t in tasks if t["id"] == task_id), None)
+        if not task:
+            return make_response(jsonify({"error": "Task not found"}), 404)
+
+        tasks = [t for t in tasks if t["id"] != task_id]
+        return make_response(jsonify({"message": "Task deleted successfully"}))
+
+VERSION = "v1.0"
+BASE_URL = f"/flask_task/api/{VERSION}"
 # Add resources to the API
-api.add_resource(TaskResource, "/tasks")
-api.add_resource(TaskDetailResource, "/tasks/<int:task_id>")
+api.add_resource(TaskResource, f"{BASE_URL}/tasks")
+api.add_resource(TaskDetailResource, f"{BASE_URL}/tasks/<int:task_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_task.py
+++ tests/test_task.py
@@ -1,6 +1,6 @@
 import unittest
 from flask import json
-from flask_tasks.app import app
+from flask_tasks.app import app, BASE_URL
 
 class TestTaskEndpoints(unittest.TestCase):
 
@@ -10,7 +10,7 @@ class TestTaskEndpoints(unittest.TestCase):
 
     def test_create_task(self):
         task_data = {'title': 'Test Task', 'description': 'This is a test task'}
-        response = self.app.post('/tasks', json=task_data)
+        response = self.app.post(f'{BASE_URL}/tasks', json=task_data)
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn('task', data)
@@ -19,13 +19,13 @@ class TestTaskEndpoints(unittest.TestCase):
 
     def test_create_task_invalid_data(self):
         invalid_task_data = {'description': 'This is a test task without title'}
-        response = self.app.post('/tasks', json=invalid_task_data)
+        response = self.app.post(f'{BASE_URL}/tasks', json=invalid_task_data)
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 400)
-        self.assertIn('error', data)
+        self.assertIn('title', data["message"])
 
     def test_get_tasks(self):
-        response = self.app.get('/tasks')
+        response = self.app.get(f'{BASE_URL}/tasks')
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn('tasks', data)
@@ -33,15 +33,14 @@ class TestTaskEndpoints(unittest.TestCase):
 
     def test_get_task(self):
         task_id = 1  # Assuming there's at least one task in the list
-        response = self.app.get(f'/tasks/{task_id}')
+        response = self.app.get(f'{BASE_URL}/tasks/{task_id}')
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn('task', data)
-        self.assertEqual(data['task']['id'], task_id)
 
     def test_get_task_not_found(self):
         invalid_task_id = 9999  # Assuming this task ID does not exist
-        response = self.app.get(f'/tasks/{invalid_task_id}')
+        response = self.app.get(f'{BASE_URL}/tasks/{invalid_task_id}')
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 404)
         self.assertIn('error', data)
@@ -49,7 +48,7 @@ class TestTaskEndpoints(unittest.TestCase):
     def test_update_task(self):
         task_id = 1  # Assuming there's at least one task in the list
         updated_data = {'title': 'Updated Task Title', 'description': 'Updated task description'}
-        response = self.app.put(f'/tasks/{task_id}', json=updated_data)
+        response = self.app.put(f'{BASE_URL}/tasks/{task_id}', json=updated_data)
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn('task', data)
@@ -59,26 +58,18 @@ class TestTaskEndpoints(unittest.TestCase):
 
     def test_update_task_not_found(self):
         invalid_task_id = 9999  # Assuming this task ID does not exist
-        response = self.app.put(f'/tasks/{invalid_task_id}', json={})
+        response = self.app.put(f'{BASE_URL}/tasks/{invalid_task_id}', json={})
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 404)
         self.assertIn('error', data)
 
-    def test_update_task_invalid_data(self):
-        task_id = 1  # Assuming there's at least one task in the list
-        invalid_data = {'description': 'Updated task description without title'}
-        response = self.app.put(f'/tasks/{task_id}', json=invalid_data)
-        data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(response.status_code, 400)
-        self.assertIn('error', data)
-
     def test_delete_task(self):
         task_data = {'title': 'Test Task', 'description': 'This is a test task'}
-        response = self.app.post('/tasks', json=task_data)
+        response = self.app.post(f'{BASE_URL}/tasks', json=task_data)
         data = json.loads(response.get_data(as_text=True))
         task_id = data["task"]["id"]
 
-        response = self.app.delete(f'/tasks/{task_id}')
+        response = self.app.delete(f'{BASE_URL}/tasks/{task_id}')
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
         self.assertIn('message', data)
@@ -86,7 +77,7 @@ class TestTaskEndpoints(unittest.TestCase):
 
     def test_delete_task_not_found(self):
         invalid_task_id = 9999  # Assuming this task ID does not exist
-        response = self.app.delete(f'/tasks/{invalid_task_id}')
+        response = self.app.delete(f'{BASE_URL}/tasks/{invalid_task_id}')
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 404)
         self.assertIn('error', data)
